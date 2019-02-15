9e0c838d12721e79b3042bf3f37ed5242a05be40
Phil Mass <philrmass@gmail.com>
2019-02-15 13:10:32 -0800

    add nav bar components

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -3,14 +3,10 @@ import styles from './NavBar.css';
 
 function NavBar() {
   return (
-    <div className={styles.navBar}>
-      <span>TriMet QuickStop </span>
+    <div className={styles.menuButton}>
+      <span>Menu</span>
     </div>
   );
 }
 
-/*
-      <TimeButton/>
-      <TimeButton/>
-      */
 export default NavBar;
