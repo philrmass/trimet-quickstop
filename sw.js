099c80d3ca60c625f122e38975acf2b8cd742eb9
Phil Mass <philrmass@gmail.com>
2019-02-17 16:34:09 -0800

    Updates

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('cache').then(function(cache) {
-      console.log('sw install');
       return cache.add('/trimet-quickstop/logo-512.png');
     })
   );
@@ -10,7 +9,6 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request).then(function(response) {
-      console.log('sw fetch');
       return response || fetch(event.request);
     })
   );
