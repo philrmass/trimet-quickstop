b966d496a0a177d9b794ccbbb470f149045c6574
Phil Mass <philrmass@gmail.com>
2019-02-22 16:23:14 -0800

    convert search pane to controlled component, add stop set

--- src/components/SearchPane.jsx
+++ src/components/SearchPane.jsx
@@ -6,36 +6,58 @@ import ResultList from './ResultList';
 import SearchBar from './SearchBar';
 import styles from './SearchPane.css';
 
-function SearchPane(props) {
-  return (
-    <div className={[styles.searchPane, (props.isOpen ? '' : styles.closed)].join(' ')}>
-      <div className={styles.closeBox}>
-        <Button
-          onClick={props.onClose}>
-          X
-        </Button>
+class SearchPane extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {stopId: ''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({stopId: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('SUBMIT', this.state.stopId);
+  }
+
+  render() {
+    return (
+      <div className={[styles.searchPane, (this.props.isOpen ? '' : styles.closed)].join(' ')}>
+        <div className={styles.closeBox}>
+          <Button
+            onClick={this.props.onClose}>
+            X
+          </Button>
+        </div>
+        <SearchBar/>
+        <div className={styles.near}>
+          Search near me
+          <Button>
+            Search
+          </Button>
+        </div>
+        <ResultList/>
+        <RecentList/>
+        <div className={styles.stopBox}>
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor='setStop'>Stop ID</label>
+            <input 
+              id='setStop' 
+              type='text'
+              value={this.state.stopId}
+              onChange={this.handleChange}/>
+            <Button
+              onClick={this.handleSubmit}>
+              Set
+            </Button>
+          </form>
+        </div>
       </div>
-      <SearchBar/>
-      <div className={styles.near}>
-        Search near me
-        <Button>
-          Search
-        </Button>
-      </div>
-      <ResultList/>
-      <RecentList/>
-      <div className={styles.stopBox}>
-        <label htmlFor="setStop">Stop ID</label>
-        <input 
-          id="setStop" 
-          type="text"/>
-        <Button
-          onClick={props.onSet}>
-          Set
-        </Button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 SearchPane.propTypes = {
